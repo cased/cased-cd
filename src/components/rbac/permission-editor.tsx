24ae5bc6f1795487080f433860a89a8350acd7c4
tnm <tnm@users.noreply.github.com>
2025-10-21 21:01:02 -0700

    fix: remove unused IconTrash import

--- src/components/rbac/permission-editor.tsx
+++ src/components/rbac/permission-editor.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { IconAdd, IconTrash } from 'obra-icons-react'
+import { IconAdd } from 'obra-icons-react'
 import { Button } from '@/components/ui/button'
 import { Badge } from '@/components/ui/badge'
 import {
